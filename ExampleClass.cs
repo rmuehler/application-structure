1978c458dc186c1deba57e0b9883d148abe921ff
Robert Muehler <robertmuehler@gmail.com>
2021-05-10 14:49:35 -0400

    completed example.

---
+++ ExampleClass.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+
+namespace application_structure
+{
+    public class ExampleClass
+    {
+        public void additionMethod() {
+            var first = 3;
+            var second = 7;
+            var result = first + second;
+        
+            result.Should().Be(10);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace application_structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var example = new ExampleClass();
+            example.additionMethod();
+
+        }
+    }
+}
